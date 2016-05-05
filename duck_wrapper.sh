f30ebc04585904f82334836d6fae8c1befe66f36
Charles Czysz <charles.czysz@gmail.com>
2016-05-05 15:39:35 +0000

    Add new file

---
+++ duck_wrapper.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+duck_list() {
+  path=$1
+  access=$2
+  secret=$3
+  duck --list griffin://$path --username $2 --password $3
+}
+
+duck_list_keys() {
+  bucket=$1
+  duck --list griffin://$bucket/ --username $2 --password $3
+}
+duck_path=$(which duck)
+
+access=$1
+secret=$2
+
+if [ ! -n "$access" ]
+then
+  echo "Please input your access key"
+  read access
+  echo "Please input your secret key"
+  read secret
+fi
+
+#echo "Please type one of the following commands, or CTRL-c to exit:"
+#echo "1. List buckets \n2. List files in specified bucket\ne. Exit"
+#read entry
+
+while :
+do
+  echo "Please type one of the following commands, or CTRL-c to exit:"
+  echo "1. List buckets \n2. List files in specified bucket\ne. Exit"
+  read entry
+  case $entry in
+    #1) echo "listing buckets";;
+    1) duck_list ''~'' $access $secret; break;;
+    2) echo "Which bucket?"; read bucket; duck_list_keys $bucket $access $secret;;
+    e) echo "Exiting";break;;
+    *) echo "Invalid argument";;
+  esac
+done
