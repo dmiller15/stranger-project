fbf7232018a099e619b691ae391e61275cf02193
Charles Czysz <charles.czysz@gmail.com>
2016-06-23 09:44:56 -0500

    pca code

--- de_analysis/pcaPlot.R
+++ de_analysis/pcaPlot.R
@@ -1,6 +1,10 @@
 source('variables.R')
 
-load()
-load()
+library(ggplot2)
+library(gplots)
 
+load(paste(cancer_type,'count.Robj', sep="_"))
+load(paste(cancer_type,'phen.Robj', sep="_"))
 
+pcaOut <- prcomp(t(count_df), scale=T)
+pcaOut <- pcaOut[,1:20]
